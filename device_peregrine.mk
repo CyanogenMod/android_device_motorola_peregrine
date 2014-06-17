85a70650a3782e12c8aa7cf2a355159d77cf0576
David Rouleau <Shabbypenguin@gmail.com>
2014-06-17 10:18:36 -0400

    Further device tree bringups

--- device_peregrine.mk
+++ device_peregrine.mk
@@ -7,16 +7,6 @@ $(call inherit-product-if-exists, vendor/motorola/peregrine/peregrine-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/motorola/peregrine/overlay
 
-LOCAL_PATH := device/motorola/peregrine
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
-else
-	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel
-
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
