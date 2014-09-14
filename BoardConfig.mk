c52d4bc923af431a6c6b79f4ebb8ac00be7e9ea2
Rabih Salamey <somcom3@gmail.com>
2014-09-14 10:32:21 -0400

    peregrine: Rename defconfig
    Change-Id: I5dd0ba8827c22df7a054513cb4d6eb9135192497

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,7 +25,7 @@ TARGET_LIBINIT_DEFINES_FILE := device/motorola/peregrine/init/init_peregrine.c
 
 # Kernel
 BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/mkbootimg.mk
-TARGET_KERNEL_CONFIG := msm8926_mmi_defconfig
+TARGET_KERNEL_CONFIG := peregrine
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 10485760
