67f07d40b2f8d66e3e1d7c292c34f1eea41c0b0e
Rabih Salamey <somcom3@gmail.com>
2014-09-21 11:26:10 -0400

    peregrine: add sd mount script on boot
       * better alternative for http://review.cyanogenmod.org/#/c/73412/
       * some sdcards don't mount on boot
       * in place of http://review.cyanogenmod.org/#/c/73716/
    
    Change-Id: Idfc22ad7085ebe62f82defbed7d9dde5566f8b08

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -7,3 +7,11 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/fstab.qcom
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := 60sdcard
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/60sdcard
+LOCAL_MODULE_PATH  := $(TARGET_OUT)/init.d
+include $(BUILD_PREBUILT)
